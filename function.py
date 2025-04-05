c761d9372d88a48c7acc1f60846e41e266af0801
mushygamer779 <akbarashurov2702@gmail.com>
2025-04-05 15:26:58 +0300

    Update function.py2

--- function.py
+++ function.py
@@ -8,10 +8,27 @@ import random
     return password'''
 
 
-def gen_pass():
-    me = int(input('how long?'))
+def gen_pass(me = 8):
+   
     elements = "+-/*!&$#?=@<>123456789"
     password = ""
     for i in range(me):
         password += random.choice(elements)
     return password
+
+
+def flip_coin():
+    return random.choice(['heads','tails'])
+    '''coin = random.choice([1,2]) 
+    result = ''
+    if coin == 1:
+        result = 'heads'
+    elif coin == 2:
+        Rresult = 'tails'
+    return result'''
+def double_letter (str):
+    result = ''
+    for letter in str:
+        result += letter * 2
+    return result
+
