c8331a202db110e6ceb2852ee556f45d6eebe255
mushygamer779 <akbarashurov2702@gmail.com>
2025-04-15 18:07:32 +0300

    Create traslator_and_caliculator.py

---
+++ traslator_and_caliculator.py
@@ -0,0 +1,10 @@
+def calculator_add(a,b):
+    return int(a) + int(b)
+def calculator_subtract(c,d):
+    return int(c) - int(d)
+def calculator_multiply(e,f):
+    return int(e) * int(f)
+def calculator_divide(g,h):
+    g = int(g)
+    h = int(h)
+    return g / h
